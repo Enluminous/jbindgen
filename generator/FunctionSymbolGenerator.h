ab0f530bfa351cc25a17c189fa62da36fa0cd290
nettal <n2lf@outlook.com>
2023-11-16 13:31:30 +0800

    add function descriptor in FunctionSymbolGenerator

--- generator/FunctionSymbolGenerator.h
+++ generator/FunctionSymbolGenerator.h
@@ -13,15 +13,19 @@
 #include "StructGeneratorUtils.h"
 
 namespace jbindgen {
-    static std::stringstream makeCoreWithoutPara(bool hasResult, const std::string& functionName, const std::string& jrtype) {
+    static std::stringstream
+    makeCoreWithoutPara(bool hasResult, const std::string &functionName, const std::string &jrtype,
+                        const std::string &functionDescriptor) {
         std::stringstream ss;
         ss << std::endl << std::endl;
         std::string invokeRet = hasResult ? "return (" + jrtype + ") " : "";
+        std::string jFunctionDescriptor = hasResult ? "FunctionDescriptor.of(" + functionDescriptor + ")" :
+                                          "FunctionDescriptor.ofVoid(" + functionDescriptor + ")";
         ss << "    private static MethodHandle " << functionName << ";" << END_LINE
            NEXT_LINE
-           << "    public static void " << functionName << "() {" << END_LINE
+           << "    public static " << (hasResult ? jrtype : "void") << " " << functionName << "() {" << END_LINE
            << "        if (" << functionName << " == null) {" << END_LINE
-           << "            " << functionName << " = GlslangSymbols.loadCFunction(" << functionName << ", " << jrtype << ".class);\n"
+           << "            " << functionName << " = GlslangSymbols.loadCFunction(" << jFunctionDescriptor << ");\n"
            << "        }\n"
            << "        try {\n"
            << "            " << invokeRet << "" << functionName << ".invoke();\n"
@@ -33,26 +37,28 @@ namespace jbindgen {
     }
 
     static std::stringstream
-    makeCoreWithPara(bool hasResult, const std::string& functionName, const std::string& jrtype, const std::string& paras,
-                     const std::string& paraNames,
-                     const std::string& paraTypes) {
+    makeCoreWithPara(bool hasResult, const std::string &functionName, const std::string &jrtype,
+                     const std::string &paras,
+                     const std::string &paraNames, const std::string &functionDescriptor) {
         std::stringstream ss;
         std::string invokeRet = hasResult ? "return (" + jrtype + ") " : "";
+        std::string jFunctionDescriptor = hasResult ? "FunctionDescriptor.of(" + functionDescriptor + ")" :
+                                          "FunctionDescriptor.ofVoid(" + functionDescriptor + ")";
+
         ss << "\n"
-        "    private static MethodHandle " << functionName << ";\n"
-        "\n"
-        "    public static " << jrtype << " " << functionName << "(" << paras << ") {\n"
-        "        if (" << functionName << " == null) {\n"
-        "            " << functionName << " = GlslangSymbols.loadCFunction(" << functionName << ", " << jrtype << ".class, " << paraTypes << ");\n"
-        "        }\n"
-        "        try {\n"
-        "            " << invokeRet << "" << functionName << ".invoke(" << paraNames << ");\n"
-        "        } catch (Throwable e) {\n"
-        "            throw new NativeFunction.InvokeException(e);\n"
-        "        }\n"
-        "    }\n"
-        "\n"
-        "";
+              "    private static MethodHandle " << functionName << ";\n""\n" <<
+           "    public static " << (hasResult ? jrtype : "void") << " " << functionName << "(" << paras << ") {\n" <<
+           "        if (" << functionName << " == null) {\n" <<
+           "            " << functionName << " = GlslangSymbols.loadCFunction(" << jFunctionDescriptor << ");\n" <<
+           "        }\n"
+           "        try {\n"
+           "            " << invokeRet << "" << functionName << ".invoke(" << paraNames << ");\n" <<
+           "        } catch (Throwable e) {\n"
+           "            throw new NativeFunction.InvokeException(e);\n"
+           "        }\n"
+           "    }\n"
+           "\n"
+           "";
         return ss;
     }
 
@@ -76,21 +82,23 @@ namespace jbindgen {
             ss << header;
             for (const auto &functionDeclaration: function_declarations) {
                 auto func = makeFunction(&functionDeclaration, userData);
+                std::stringstream funcTypes;
+                for (const auto &descriptor: func.functionDescriptors) {
+                    //TODO: fd,fd,fd
+                    funcTypes << " " << descriptor;
+                }
                 if (func.invokeParameters.empty()) {
-                    ss << makeCoreWithoutPara(func.hasResult, func.functionName, func.jResult).str();
+                    ss << makeCoreWithoutPara(func.hasResult, func.functionName, func.jResult, funcTypes.str()).str();
                 } else {
                     std::stringstream jparas;
                     for (const auto &pars: func.jParameters) {
+//                        para,para,para
                         jparas << " " << pars;
                     }
                     std::stringstream invpara;
                     for (const auto &para: func.invokeParameters) {
+//                        para,para,para
                         invpara << " " << para;
-                    }
-                    std::stringstream funcTypes;
-                    for (const auto &descriptor: func.functionDescriptors) {
-                        //TODO:
-                        funcTypes << " " << descriptor;
                     }
                     ss << makeCoreWithPara(func.hasResult, func.functionName, func.jResult, jparas.str(), invpara.str(),
                                            funcTypes.str()).str();
