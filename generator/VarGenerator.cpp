726b3be5db4260b945ba3adcf00a2ee65fc63870
nettal <n2lf@outlook.com>
2023-11-26 20:20:51 +0800

    add empty VarGenerator

---
+++ generator/VarGenerator.cpp
@@ -0,0 +1,8 @@
+//
+// Created by nettal on 23-11-26.
+//
+
+#include "VarGenerator.h"
+
+namespace jbindgen {
+} // jbindgen
---
+++ generator/VarGenerator.h
@@ -0,0 +1,16 @@
+//
+// Created by nettal on 23-11-26.
+//
+
+#ifndef JAVABINDGEN_VARGENERATOR_H
+#define JAVABINDGEN_VARGENERATOR_H
+
+namespace jbindgen {
+
+    class VarGenerator {
+
+    };
+
+} // jbindgen
+
+#endif //JAVABINDGEN_VARGENERATOR_H
