9f7a5c61a562a515af38d3deb45aa2cf6380794d
SnowNF <SnowNF@outlook.com>
2023-12-04 23:22:27 +0800

    improve test path.

--- tests/testFreeType.cpp
+++ tests/testFreeType.cpp
@@ -1,16 +1,16 @@
 #include "../analyser/Analyser.h"
 #include "../generator/Generator.h"
 
-#ifndef CURRENT_BUILD_DIR
-#error CURRENT_BUILD_DIR not defined
+#ifndef TEST_SRC_DIR
+#error TEST_SRC_DIR not defined
 #endif
 
 //
 // Created by nettal on 23-12-4.
 //
 int main() {
-    const char* args[] = {"-I", CURRENT_BUILD_DIR"/tests/freetype/src/freetype-git/include/","-I","/usr/include"};
-    jbindgen::AnalyserConfig config = jbindgen::defaultAnalyserConfig(CURRENT_BUILD_DIR"/tests/freetype/src/freetype-git/include/freetype/freetype.h", args,
+    const char* args[] = {"-I", TEST_SRC_DIR"/include/","-I","/usr/include"};
+    jbindgen::AnalyserConfig config = jbindgen::defaultAnalyserConfig(TEST_SRC_DIR"/include/freetype/freetype.h", args,
                                                                       4);
     config.acceptedPath="freetype";
     jbindgen::Analyser analysed(config);
--- tests/testMiniaudio.cpp
+++ tests/testMiniaudio.cpp
@@ -5,13 +5,13 @@
 #include "../analyser/Analyser.h"
 #include "../generator/Generator.h"
 
-#ifndef CURRENT_BUILD_DIR
-#error CURRENT_BUILD_DIR not defined
+#ifndef TEST_SRC_DIR
+#error TEST_SRC_DIR not defined
 #endif
 
 int main() {
     const char *args[] = {"-I", "/usr/include"};
-    std::string headPath = CURRENT_BUILD_DIR"/tests/miniaudio/src/miniaudio-git/extras/miniaudio_split/miniaudio.h";
+    std::string headPath = TEST_SRC_DIR"/extras/miniaudio_split/miniaudio.h";
     jbindgen::Analyser analysed(jbindgen::defaultAnalyserConfig(headPath, args, 2));
     jbindgen::Generator generator(
             jbindgen::defaultGeneratorConfig("./generation/miniaudio", "miniaudio", "miniaudio", analysed));
