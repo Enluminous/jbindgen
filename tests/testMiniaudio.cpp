e93d0c283165969b2597e96091f482ae5c152a3a
SnowNF <SnowNF@outlook.com>
2023-12-04 15:15:53 +0800

    add miniaudio tests

---
+++ tests/testMiniaudio.cpp
@@ -0,0 +1,16 @@
+//
+// Created by snownf on 23-12-4.
+//
+#include <iostream>
+#include "../analyser/Analyser.h"
+#include "../generator/Generator.h"
+
+int main() {
+    const char *args[] = {"-I", "/usr/include"};
+    std::string headPath = CURRENT_BUILD_DIR"/tests/miniaudio/src/miniaudio-git/extras/miniaudio_split/miniaudio.h";
+    jbindgen::Analyser analysed(jbindgen::defaultAnalyserConfig(headPath, args, 2));
+    jbindgen::Generator generator(
+            jbindgen::defaultGeneratorConfig("./generation/miniaudio", "miniaudio", "miniaudio", analysed));
+    generator.generate();
+    return 0;
+}
