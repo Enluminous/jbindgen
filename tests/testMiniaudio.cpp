fa590f887bcb7351c10b241f6faec62ca8ad2c4e
nettal <n2lf@outlook.com>
2023-12-04 15:37:14 +0800

    check CURRENT_BUILD_DIR marco

--- tests/testMiniaudio.cpp
+++ tests/testMiniaudio.cpp
@@ -5,6 +5,10 @@
 #include "../analyser/Analyser.h"
 #include "../generator/Generator.h"
 
+#ifndef CURRENT_BUILD_DIR
+#error CURRENT_BUILD_DIR not defined
+#endif
+
 int main() {
     const char *args[] = {"-I", "/usr/include"};
     std::string headPath = CURRENT_BUILD_DIR"/tests/miniaudio/src/miniaudio-git/extras/miniaudio_split/miniaudio.h";
