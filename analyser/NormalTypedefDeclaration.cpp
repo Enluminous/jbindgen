8c22eb02e91b598aaf77db1dbf30f9d96eb1ac4d
nettal <n2lf@outlook.com>
2023-12-05 10:37:10 +0800

    fix typedef not visit all its children

--- analyser/NormalTypedefDeclaration.cpp
+++ analyser/NormalTypedefDeclaration.cpp
@@ -16,32 +16,17 @@ namespace jbindgen {
         auto analyser = reinterpret_cast<Analyser *>(client_data);
         if (c.kind == CXCursor_ParmDecl) {
             analyser->visitTypeDefFunction(parent);
-            return CXChildVisit_Break;
         }
         if (c.kind == CXCursor_StructDecl) {
             analyser->visitStruct(c);
-            return CXChildVisit_Break;
         }
         if (c.kind == CXCursor_UnionDecl) {
             analyser->visitUnion(c);
-            return CXChildVisit_Break;
-        }
-        if (c.kind == CXCursor_TypeRef) {
-            return CXChildVisit_Break;
-        }
-        if (c.kind == CXCursor_IntegerLiteral) {
-            //declare array
-            return CXChildVisit_Break;
-        }
-        if (c.kind == CXCursor_AlignedAttr) {
-            //?
-            return CXChildVisit_Break;
         }
         if (c.kind == CXCursor_EnumDecl) {
             analyser->visitEnum(c);
-            return CXChildVisit_Break;
         }
-        return CXChildVisit_Break;
+        return CXChildVisit_Continue;
     }
 
     std::string const NormalTypedefDeclaration::getName() const {
@@ -68,7 +53,7 @@ namespace jbindgen {
                                          mappedType, c));
         clang_visitChildren(c, NormalTypedefDeclaration::visitChildren, &analyser);
         if (!analyser.getCXCursorMap().contains(c)) {
-            analyser.updateCXCursorMap(c,shared_ptr);
+            analyser.updateCXCursorMap(c, shared_ptr);
         }
         analyser.visitCXType(mappedType);
         return shared_ptr;
