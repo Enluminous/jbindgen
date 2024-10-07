2a90bf519336fbbec19b057cc29948f0e9198bb9
SnowNF <SnowNF@outlook.com>
2024-10-07 21:00:49 +0800

    disable clang crash recovery

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,38 @@ import generator.Generator;
 import libclang.*;
 import preprocessor.Preprocessor;
 
-import java.lang.foreign.Arena;
-import java.lang.foreign.SymbolLookup;
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
+    private static void disableClangCrashRecovery() {
+        Linker linker = Linker.nativeLinker();
+        try (Arena mem = Arena.ofConfined()) {
+            MemorySegment argv1 = mem.allocateFrom("LIBCLANG_DISABLE_CRASH_RECOVERY");
+            MemorySegment argv2 = mem.allocateFrom("1");
+
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                Optional<MemorySegment> putenvHandle = linker.defaultLookup().find("SetEnvironmentVariable");
+                MethodHandle putenvFunc = linker.downcallHandle(putenvHandle.orElseThrow(), FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS));
+                int _ = (int) putenvFunc.invokeExact(argv1, argv2);
+            } else {
+                Optional<MemorySegment> putenvHandle = linker.defaultLookup().find("setenv");
+                MethodHandle putenvFunc = linker.downcallHandle(putenvHandle.orElseThrow(), FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.JAVA_INT));
+                int _ = (int) putenvFunc.invokeExact(argv1, argv2, 1);
+            }
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
+        disableClangCrashRecovery();
+
         LibclangSymbols.addSymbols(SymbolLookup.libraryLookup("libclang-17.so.1", Arena.global()));
         var analyser = new Analyser("test/miniaudio.h", List.of("-I", "/usr/include"));
 
