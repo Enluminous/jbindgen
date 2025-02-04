3bcb14e0b213d53b1f3bf190ae43e3e6d303dab8
nettal <n2lf@outlook.com>
2025-02-04 20:06:07 +0800

    add hashset to avoid duplicated writing

--- src/generator/Utils.java
+++ src/generator/Utils.java
@@ -6,15 +6,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.regex.Pattern;
 
 public class Utils {
     private static final Pattern CLASS_NAME = Pattern.compile("^[a-zA-Z0-9_$]*$");
     private static final Pattern VAR_NAME = Pattern.compile("^[a-zA-Z_$][a-zA-Z\\d_$]*$");
 
+    private static final HashSet<Path> writingPaths = new HashSet<>();
+
     public static void write(Path path, String content) {
         try {
             Files.createDirectories(path.getParent());
+            if (writingPaths.contains(path.toAbsolutePath())) {
+                throw new RuntimeException("Path " + path.toAbsolutePath() + " already written");
+            }
+            writingPaths.add(path.toAbsolutePath());
             FileWriter fileWriter = new FileWriter(path.toFile(), false);
             fileWriter.write(content);
             fileWriter.close();
@@ -25,7 +32,7 @@ public class Utils {
 
     public static String makeMemoryLayout(long bytes) {
         return "MemoryLayout.structLayout(MemoryLayout.sequenceLayout(" + bytes + ", "
-               + CommonTypes.Primitives.JAVA_BYTE.getMemoryLayout() + "))";
+                + CommonTypes.Primitives.JAVA_BYTE.getMemoryLayout() + "))";
     }
 
     public static boolean isValidClassName(String className) {
